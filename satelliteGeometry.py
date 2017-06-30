c7fd45168b76b5ecfcd5e65548673c7aabc5ce84
Tristan Quaife <tquaife@gmail.com>
2017-06-30 12:35:33 +0100

    First commit

---
+++ satelliteGeometry.py
@@ -0,0 +1,13 @@
+
+class sensorGeometry:
+
+  def __init__(self):
+    """Class to hold sun-sensor geometry information
+    """
+
+    self.vza=None
+    self.vaa=None
+    self.sza=None
+    self.saa=None
+
+
---
+++ stateVector.py
@@ -0,0 +1,17 @@
+
+
+class stateVector:
+
+  def __init__(self):
+    """Class to hold state vector data for optical
+    and microwave canopy RT models
+    """
+    
+    self.lai=None
+    self.canHeight=None
+    self.leafChl=None
+    self.leafWater=None
+    self.soilMoisture=None
+    self.soilAlbedo=None
+    
+
